c59fca05ec68663c1b4e84020bcad5e411c486a3
sarayasser200 <saraelgazaly2004@gmail.com>
2025-06-23 15:58:16 +0300

    hahhha

--- src/libs/utils/dotenv.ts
+++ src/libs/utils/dotenv.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 // Initializing dotenv
 const envPath: string = path.resolve(
   process.cwd() +
-    (process.env.NODE_ENV === 'developement' ? '/.env' : '/.env.test'),
+    (process.env.NODE_ENV === 'developement' ? '/.env' : '/.env'),
 );
 
 config({ path: envPath });
